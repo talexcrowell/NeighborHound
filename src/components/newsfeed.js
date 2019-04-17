5c509d4e802eb04dfe8af06796a4771bfff4b495
talexcrowell <talexcrowell@gmail.com>
2019-04-17 16:42:30 -0700

    moved source and refigured date/time

--- src/components/newsfeed.js
+++ src/components/newsfeed.js
@@ -29,13 +29,14 @@ export function ContentNewsFeedItem (props) {
       <li className='article' key ={props.index}>
         <div className='article-img-container'>
           <img className='article-img' src={props.article.urlToImage} alt={props.article.title} />
+          <p className='article-source'>{props.article.source.name}</p>
         </div>
+
           <a className='click-area' target="_blank" rel="noopener noreferrer" href={props.article.url}>
             <section className='article-description'>
               <p className='article-title'>{normalizeTitle(props.article.title)}</p>
-              <p className='article-detail'>{props.article.source.name}</p>
-              <p className='article-detail'>{normalizeTime(props.article.publishedAt)}</p>
-              <p className='article-detail'>{normalizeDate(props.article.publishedAt)}</p>
+              <p className='article-time'>@: {normalizeTime(props.article.publishedAt)}</p>
+              <p className='article-date'>{normalizeDate(props.article.publishedAt)}</p>
             </section>
             <section className='article-content'>
               <p className='article-summary'>{props.article.content ? normalizeSummary(props.article.content) : 'No summary available from '+ props.article.source.name}</p>
