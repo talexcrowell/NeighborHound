dc4d70595f5897f8b0324ee99ef8b4f2085e5cae
talexcrowell <talexcrowell@gmail.com>
2019-04-17 20:50:29 -0700

    ready

--- src/components/communityfeed.js
+++ src/components/communityfeed.js
@@ -5,15 +5,15 @@ export function ContentCommunityFeedItem (props) {
 
     return(
       <li className='post' key ={props.index}>
-        <div className='post-source'>reddit.com</div>
+        <section className='post-description'>
+          <h3 className='post-title'>{props.post.title}</h3>
+        </section>
+        <div className='post-source'>{props.post.source}</div>
         <div className='post-interest'></div>
         <a target="_blank" rel="noopener noreferrer" href={props.post.url}>
           <div className='img-container'>
             <img className='post-img' src={props.post.img} alt={props.post.title} />
           </div>
-          <section className='post-description'>
-            <h3 className='post-title'>{props.post.title}</h3>
-          </section>
         </a>
       </li>
     )
