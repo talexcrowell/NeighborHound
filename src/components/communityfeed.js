c93b3ef1286f2005e60c59fda27a1e9cc0c3c062
talexcrowell <talexcrowell@gmail.com>
2019-04-19 17:45:26 -0700

    finished working with media in community

--- src/components/communityfeed.js
+++ src/components/communityfeed.js
@@ -2,25 +2,42 @@ import React from 'react';
 import {connect} from 'react-redux';
 
 export function ContentCommunityFeedItem (props) {
-    // let imgRegex= /\.mp4*/g;
-    // if(imgRegex(props.post.img)[0] !== null){
-    //   img= (<video className='post-img' controls> 
-    //           <source src={props.post.img} type='video/mp4' /> 
-    //         </video>);
-    // }
-    let img= (<img className='post-img' src={props.post.img} alt={props.post.title} />);
+    let media;
+    if(props.post.type === 'video/mp4' || props.post.type === 'video/webm'){
+      media = (
+        <a target="_blank" rel="noopener noreferrer" href={props.post.url}>
+        <div className='img-container'>
+          <video autoPlay loop muted controls className='post-mp4'>
+            <source src={props.post.img} type={props.post.type} />
+          </video>
+        </div>
+      </a>);
+    } 
+    else if(props.post.type === 'article'){
+      media = (
+        <a target="_blank" rel="noopener noreferrer" href={props.post.url}>
+          <div className='post-article-container'>
+            <img className='post-article-img' src='https://i.imgur.com/ty6YjaL.png' alt={props.post.title} />
+          </div>
+        </a>);
+    }
+    else{
+      media = (
+        <a target="_blank" rel="noopener noreferrer" href={props.post.url}>
+          <div className='img-container'>
+            <img className='post-img' src={props.post.img} alt={props.post.title} />
+          </div>
+        </a>);
+    }
+
     return(
       <li className='post' key ={props.index}>
         <section className='post-description'>
           <h3 className='post-title'>{props.post.title}</h3>
         </section>
         <div className='post-source'>{props.post.source}</div>
-        <div className='post-interest'></div>
-        <a target="_blank" rel="noopener noreferrer" href={props.post.url}>
-          <div className='img-container'>
-            {img}
-          </div>
-        </a>
+        <div className='post-category'>{props.post.category}</div>
+        {media}
       </li>
     )
 }
